4b4e8588a672b35f0d80f40305784786f120e97d
Moin Virani <moin.virani@gmail.com>
2013-11-14 15:04:08 -0500

    Added images to product view page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,13 +2,20 @@ require 'open-uri'
 
 class ProductsController < ApplicationController
   def index
-  	products_json = open('http://lcboapi.com/products').read
+  	products_json = open('http://lcboapi.com/products?per_page=35').read
   	@products = JSON.parse(products_json)
   end
 
   def show
-  	
+  	product_json = open("http://lcboapi.com/products/#{params['id']}").read
+  	@product = JSON.parse(product_json) ['result']
   end
+		# id = params[:id]
+	 #  	products_json = open('http://lcboapi.com/products?per_page=35').read
+	 #  	products = JSON.parse(products_json)
+	 #  	products['result'].each do |product|
+	 #  		@product = product if product['id'].to_i == id.to_i
+	 
 
   def new
   end
